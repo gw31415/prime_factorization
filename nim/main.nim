08e5f2caf1b4fff6a634a5995a894f2ba43a476b
Gart2357 <57865205+Gart2357@users.noreply.github.com>
2020-06-29 01:09:11 +0900

    len を使ったら 0.03s 速くなりました（白目）

--- nim/main.nim
+++ nim/main.nim
@@ -8,10 +8,10 @@ for n in 1..100000:
     arr = @[]
     temp = n
     for i in 2..int(sqrt(n.float) + 1):
-        while temp mod i==0:
+        while temp mod i == 0:
             arr.add(i)
             temp = temp div i
-    if temp!=1:
+    if temp != 1:
         arr.add(temp)
-    if arr == @[]:
+    if len(arr) == 0:
         arr.add(n)
